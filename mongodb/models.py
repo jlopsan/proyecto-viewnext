a17f27195e1ee50f2c1ab1159027302ea66bd8f1
RafaelT00 <92472445+RafaelT00@users.noreply.github.com>
2025-02-06 15:41:15 +0100

    Update models.py

--- mongodb/models.py
+++ mongodb/models.py
@@ -5,33 +5,33 @@ from datetime import datetime
 
 #Clase Consulta para gestionar correctamente las consultas
 class Consulta(BaseModel):
-    id: str = Field(default_factory=uuid.uuid4, alias="_id")
-    usuario: str = Field(...)
+    uuid: str
     consulta: str = Field(...)
+    respuesta: str = Field(...)
     fecha: datetime = Field(...)
 
     class Config:
         populate_by_name = True
         json_schema_extra = {
             "test": {
-                "_id": "12345678-1234-5678-1234-567812345678",
-                "usuario": "test_user",
+                "uuid": "EscuelasViewnextIA-JLS-12345678",
                 "consulta": "Consulta becas",
+                "respuesta": "Para solicitar una beca...",
                 "fecha": datetime(2024, 4, 4)
             }
         }
 
 #Clase ConsultaUpdate para getionar las actualizaciones de las consultas
 class ConsultaUpdate(BaseModel):
-    usuario: Optional[str]
     consulta: Optional[str]
+    respuesta: Optional[str]
     fecha: Optional[datetime]
 
     class Config:
         json_schema_extra = {
             "test": {
-                "usuario": "test_user",
                 "consulta": "Consulta becas",
+                "respuesta": "Para solicitar una beca...",
                 "fecha": datetime(2022, 2, 2)
             }
         }
